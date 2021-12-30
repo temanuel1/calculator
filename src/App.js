cf3f2fe1770a6da5eaf01c8d1647bacc0f8a1102
Tyler Emanuel <tyleremanuel23@gmail.com>
2021-12-30 17:53:24 -0600

    updated color scheme

--- src/App.js
+++ src/App.js
@@ -32,50 +32,54 @@ const App = () => {
     setResult("")
   }
 
-  const color = "#79A7D3"
+  const color1 = "#ff8800"
+  const color2 = '#ff9721'
+  const color3 = '#ffa238'
+  const color4 = '#ffac4d'
+  const color5 = '#ffba6b'
   
   return (
     <div className="App">
       <div className = 'calc-wrapper'>
         < Input text = {text} result = {result}/>
         <div className = 'row'>
-          <Button className = "button" symbol=" deg" color={color} handleClick={addToText} />
-          <Button className = "button" symbol="(" color={color} handleClick={addToText} />
-          <Button className = "button" symbol=")" color={color} handleClick={addToText} />
-          <Button className = "button" symbol="sqrt" color={color} handleClick={addPar} />
-          <Button className = "button" symbol="^" color={color} handleClick={addToText} />
+          <Button className = "button" symbol=" deg" color={color1} handleClick={addToText} />
+          <Button className = "button" symbol="(" color={color1} handleClick={addToText} />
+          <Button className = "button" symbol=")" color={color1} handleClick={addToText} />
+          <Button className = "button" symbol="sqrt" color={color1} handleClick={addPar} />
+          <Button className = "button" symbol="^" color={color1} handleClick={addToText} />
         </div>
         <div className = 'row'>
-          <Button className = "button" symbol="sin" color={color} handleClick={addPar} />
-          <Button className = "button" symbol="7" color={color} handleClick={addToText}/>
-          <Button className = "button" symbol="8" color={color} handleClick={addToText}/>
-          <Button className = "button" symbol="9" color={color} handleClick={addToText}/>
-          <Button className = "button" symbol="/" color={color} handleClick={addToText}/>
+          <Button className = "button" symbol="sin" color={color2} handleClick={addPar} />
+          <Button className = "button" symbol="7" color={color2} handleClick={addToText}/>
+          <Button className = "button" symbol="8" color={color2} handleClick={addToText}/>
+          <Button className = "button" symbol="9" color={color2} handleClick={addToText}/>
+          <Button className = "button" symbol="/" color={color2} handleClick={addToText}/>
         </div>
         <div className = 'row'>
-          <Button className = "button" symbol="cos" color={color} handleClick={addPar} />
-          <Button className = "button" symbol="4" color={color} handleClick={addToText}/>
-          <Button className = "button" symbol="5" color={color} handleClick={addToText}/>
-          <Button className = "button" symbol="6" color={color} handleClick={addToText}/>
-          <Button className = "button" symbol="*" color = {color} handleClick={addToText}/>
+          <Button className = "button" symbol="cos" color={color3} handleClick={addPar} />
+          <Button className = "button" symbol="4" color={color3} handleClick={addToText}/>
+          <Button className = "button" symbol="5" color={color3} handleClick={addToText}/>
+          <Button className = "button" symbol="6" color={color3} handleClick={addToText}/>
+          <Button className = "button" symbol="*" color = {color3} handleClick={addToText}/>
         </div>
         <div className = 'row'>
-          <Button className = "button" symbol="tan" color={color} handleClick={addPar} />
-          <Button className = "button" symbol="1" color={color} handleClick={addToText}/>
-          <Button className = "button" symbol="2" color={color} handleClick={addToText}/>
-          <Button className = "button" symbol="3" color={color} handleClick={addToText}/>
-          <Button className = "button" symbol="+" color = {color} handleClick={addToText}/>
+          <Button className = "button" symbol="tan" color={color4} handleClick={addPar} />
+          <Button className = "button" symbol="1" color={color4} handleClick={addToText}/>
+          <Button className = "button" symbol="2" color={color4} handleClick={addToText}/>
+          <Button className = "button" symbol="3" color={color4} handleClick={addToText}/>
+          <Button className = "button" symbol="+" color = {color4} handleClick={addToText}/>
         </div>
         <div className = 'row'>
-          <Button className = "button" symbol="log" color={color} handleClick={addPar} />
-          <Button className = "button" symbol="π" color={color} handleClick={addPi} />
-          <Button className = "button" symbol="0" color={color} handleClick={addToText}/>
-          <Button className = "button" symbol="." color={color} handleClick={addToText}/>          
-          <Button className = "button" symbol="-" color = {color} handleClick={addToText}/>
+          <Button className = "button" symbol="log" color={color5} handleClick={addPar} />
+          <Button className = "button" symbol="π" color={color5} handleClick={addPi} />
+          <Button className = "button" symbol="0" color={color5} handleClick={addToText}/>
+          <Button className = "button" symbol="." color={color5} handleClick={addToText}/>          
+          <Button className = "button" symbol="-" color = {color5} handleClick={addToText}/>
         </div>
         <div className = "bottom-row">
-        <Button className = "button" symbol="Clear" color = "#8A307F" handleClick={resetInput}/>
-        <Button className = "button" symbol="=" color="#6883BC" handleClick={calculateResult}/>
+        <Button className = "button" symbol="Clear" color = "#ffcd94" handleClick={resetInput}/>
+        <Button className = "button" symbol="=" color="#ffcd94" handleClick={calculateResult}/>
         </div>
       </div>
     </div>
